178b1c647518b98ce2d67d1eea10538ed7bf34d5
hoang466a <hoangtn96@gmail.com>
2021-12-20 11:07:47 +0700

    day bai tap ngay 20/12/2021

--- module2/src/ss13_sapxep/baitap/InsertionSortInt.java
+++ module2/src/ss13_sapxep/baitap/InsertionSortInt.java
@@ -5,32 +5,35 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 
-public class InsertionSort {
+public class InsertionSortInt {
 
 
     public static void main(String[] args) {
         int pos;
-        double x;
-        double[] array={5,3,1,7,3,1,8,9,1,6,7,1,4,8,4,5,10};
-
-
+        int x;
+        int[] array={5,3,2,7,3,1,8,9,11,6,7,5,4,8,4,5,10};
+        System.out.println("Array before sort: ");
+        for (int arr:array)
+        {
+            System.out.print(arr+" ");
+        }
         for(int i = 1; i < array.length; i++){ //đoạn array[0] đã sắp xếp
             x = array[i];
             pos = i;
             while(pos > 0 && x < array[pos-1]){
-                System.out.println("chance place of "+array[pos]+" to "+array[pos-1]);
                 array[pos] = array[pos-1]; // đổi chỗ
                 pos--;
             }
             array[pos] = x;
-            System.out.println("array after loop: ");
-            for (double arr:array)
-            {
-                System.out.print(arr+ " ");
-            }
-            System.out.println("");
         }
 
+
+        System.out.println(" ");
+        System.out.println("Array after sort: ");
+        for (int arr:array)
+        {
+            System.out.print(arr+" ");
+        }
 
 
     }
