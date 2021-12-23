ca7ec59aa32a683c8fc6c6e89075e8f07bffafcd
hoang466a <hoangtn96@gmail.com>
2021-12-23 11:53:42 +0700

    day bai tap ngay 23/12/2021

--- module2/src/ss15_textfile/baitap/CopyToTargerFile.java
+++ module2/src/ss15_textfile/baitap/CopyToTargerFile.java
@@ -1,50 +1,67 @@
 package ss15_textfile.baitap;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-public class CopyToTargerFile {
+import java.io.*;
 
-    public int countCharacter(String file)
-    {
+public class CopyToTargerFile {
+public static final String File_source="D:\\Codegym\\Module02\\module2\\src\\ss15_textfile\\baitap\\sourcefile";
+public static final String File_target="D:\\Codegym\\Module02\\module2\\src\\ss15_textfile\\baitap\\targetfile";
+
+
+public int countCharacter(String file) throws FileNotFoundException {
+        BufferedReader bw=null;
         int count=0;
         try{
-            BufferedReader bw=new BufferedReader(new FileReader(file));
+            bw=new BufferedReader(new FileReader(file));
             int i;
             while((i=bw.read())!=-1)
             {
                 if ((char)i!=' ')
-                {
                     count++;
-                }
             }
 
         }
         catch(Exception e) {
             System.err.println("Fie không tồn tại or nội dung có lỗi!");
-    }
+            }
+        finally {
+            try {
+                bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         return count;
     }
 
-    public void countCharacter1(String source,String target)
+
+
+
+    public void copyToTargetFile(String source,String target) throws IOException
     {
-        int count=0;
+        BufferedReader br=null;
+        FileWriter bw=null;
+        BufferedWriter bw1=null;
+        String result="";
         try{
-            BufferedReader br=new BufferedReader(new FileReader(source));
-            FileWriter bw=new FileWriter(target, true);
-            BufferedWriter bw1=new BufferedWriter(bw);
+            br=new BufferedReader(new FileReader(source));
+            bw=new FileWriter(target);
+            bw1=new BufferedWriter(bw);
             int i;
             while((i=br.read())!=-1)
             {
-                if ((char)i!=' ')
-                {
-                    count++;
-                }
+                result+=(char)i;
             }
+            bw1.write(result);
+
+
 
         }
         catch(Exception e) {
             System.err.println("Fie không tồn tại or nội dung có lỗi!");
+        }
+        finally {
+            br.close();
+            bw1.flush();
+            bw1.close();
         }
 
     }
@@ -53,7 +70,15 @@ public class CopyToTargerFile {
 
     public static void main(String[] args) {
         CopyToTargerFile check=new CopyToTargerFile();
-        System.out.println("Số lượng từ trong file gốc là: "+check.countCharacter("D:\\Codegym\\Module02\\module2\\src\\ss15_textfile\\baitap\\sourcefile"));
+        try {
+            System.out.println("Số lượng từ trong file gốc là: "+check.countCharacter(File_source));
+            check.copyToTargetFile(File_source,File_target);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
 
     }
 
